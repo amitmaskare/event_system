c1657d3804529a06b8eaaaf98a0c32b6348ad9cc
amitmaskare <65808453+amitmaskare@users.noreply.github.com>
2025-09-02 11:44:57 -0700

    approval

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -16,8 +16,9 @@ class Dashboard extends CI_Controller
 
 	public function registration_list()
 	{
-		$cond="band.role='".$this->session->userdata('role')."'";	
+		$cond="band.role='".$this->session->userdata('role')."' and reg.user_id!='".$this->session->userdata('userId')."'";	
 		$data['approver']=$this->Approvermodel->registration_list($cond);
+		echo "<pre>"; print_r($data['approver']); exit;
 		$this->load->view('approver/registration_list',$data);
 	}
 
--- application/models/Approvermodel.php
+++ application/models/Approvermodel.php
@@ -6,11 +6,12 @@ class Approvermodel extends CI_Model
 
 	public function registration_list($cond)
 	{
-		 $this->db->select('reg.*,e.name,band.id as band_id,band.role as band_role');
-        $this->db->from('registrations as reg');
-        $this->db->join('approval_bands as band','band.event_id=reg.event_id','left');
-        $this->db->join('approval_bands as band','band.event_id=reg.event_id','left');
-        $this->db->join('events as e','e.id=band.event_id','left');
+		 $this->db->select('reg.*,e.name,band.id as band_id,band.role as band_role,band.band_order,u.name as user_name');
+        $this->db->from('approvals as a');
+        $this->db->join('registrations as reg','reg.id=a.registration_id');
+        $this->db->join('approval_bands as band','band.id=a.band_id');
+        $this->db->join('events as e','e.id=reg.event_id');
+        $this->db->join('users as u','u.id=reg.user_id');
         $this->db->where($cond);
         $query = $this->db->get();
         return $query->result();
